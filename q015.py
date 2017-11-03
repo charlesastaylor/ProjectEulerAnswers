15db444bea37c85ab92071af76e38232a0f5a752
Charles Taylor <charlesastaylor@gmail.com>
2017-11-03 22:41:59 +0000

    requirements.txt and updated q15

--- q015.py
+++ q015.py
@@ -1,16 +1,17 @@
+"""Problem 15 - Lattice paths."""
+LIMIT = 3
+counter = 0
 
-LIMIT = 2
-
-def move(pos, counter):
+def move(pos):
+    global counter # LUL
     if pos == [LIMIT, LIMIT]:
         counter += 1
         return
     elif pos[0] > LIMIT or pos[1] > LIMIT:
         return
     else:
-        move([pos[0], pos[1]+1], counter)
-        move([pos[0]+1, pos[1]], counter)
+        move([pos[0], pos[1]+1])
+        move([pos[0]+1, pos[1]])
 
-counter=0
-move([0,0], counter)
+move([0, 0])
 print(counter)
