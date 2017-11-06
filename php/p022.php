a1bbfcb5c89110800f420160ab4139f129068191
Charles Taylor <charlesastaylor@gmail.com>
2017-11-06 15:23:54 +0000

    p22 not quite working

--- php/p022.php
+++ php/p022.php
@@ -5,6 +5,18 @@
  */
 
 $file = fopen("p022_names.txt", "r");
-
-var_dump($file);
-
+$names_string = fgets($file);
+$names_array = explode(",", $names_string);
+sort($names_array) or die("sort failed");
+$total = 0;
+foreach($names_array as $key => $name) {
+    $value = 0;
+    echo $key . " " . $name . " ";
+    foreach(str_split($name) as $char) {
+        $value += ord($char) - 96;
+        echo $char . " " . $value . " | ";
+    }
+    $total += $value * ($key + 1);
+    // echo $total;
+}
+echo $total;
